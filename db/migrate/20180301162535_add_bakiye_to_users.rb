b9f2029ebfff8fedf44abff74a0076094f0d3f16
ylmazmehmet60 <170541077@firat.edu.tr>
2018-03-01 16:42:57 +0000

    degisiklik2

--- db/migrate/20180301162535_add_bakiye_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddBakiyeToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :bakiye, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 20180301163542) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.float    "bakiye"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
