ac17348e2473889c6ce2df8220ac8ae9c2db3020
ylmazmehmet60 <170541077@firat.edu.tr>
2018-03-01 16:26:33 +0000

    degisiklik

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301162010) do
+ActiveRecord::Schema.define(version: 20180301162535) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "bdy"
